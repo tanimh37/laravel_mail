586780be2783853e40217b4c89140fe12ea5cacb
Tanim Hasan <tanimh37@gmail.com>
2024-12-12 12:52:34 +0600

    first commit

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Mail;
+
+use App\Mail\DemoMail;
+
+
+class MailController extends Controller
+{
+    /**
+
+     * Write code on Method
+
+     *
+
+     * @return response()
+
+     */
+    public function index()
+
+    {
+
+        $mailData = [
+
+            'title' => 'Mail from ItSolutionStuff.com',
+
+            'body' => 'This is for testing email using smtp.'
+
+        ];
+
+        Mail::to('nazmuljoys@gmail.com')->send(new DemoMail($mailData));
+
+        dd("Email is sent successfully.");
+
+    }
+
+}
